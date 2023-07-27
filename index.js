91241b8eefcb7aa610f117bc969413592e9db90e
kyan0045 <84374752+kyan0045@users.noreply.github.com>
2023-07-27 21:24:22 +0200

    v1.2.1 - Added multi-server catching
    Added blacklistedGuilds

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ async function Login(token, Client, guildId) {
   client.on("messageCreate", async (message) => {
     if (
       message.guild?.id == guildId || !guildId &&
-      message.author.id == "716390085896962058"
+      message.author.id == "716390085896962058" && !config.blacklistedGuilds.includes(message.guild?.id)
     ) {
       const messages = await message.channel.messages
         .fetch({ limit: 2, around: message.id })
