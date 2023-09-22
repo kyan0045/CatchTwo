836f46c76fdb307f80889c67156be2cfd16412d7
Kyan <84374752+kyan0045@users.noreply.github.com>
2023-09-23 01:07:06 +0200

    v1.2.9 - Added some extra sleep logging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var version = "1.2.8";
-// Version 1.2.8
+var version = "1.2.9";
+// Version 1.2.9
 // EVERYTHING can be set up in config.json, no need to change anything here :)!
 
 const { Client, Permissions } = require("discord.js-selfbot-v13");
@@ -62,7 +62,7 @@ axios
                   "**\nNew version: **" +
                   v +
                   "**\nPlease update: " +
-                  "https://github.com/Kyan0045/CatchTwo"
+                  "https://github.com/kyan0045/CatchTwo"
               )
               .setColor("#E74C3C")
           );
@@ -102,10 +102,10 @@ if (process.env.REPLIT_DB_URL && (!process.env.TOKENS || !process.env.CONFIG))
   );
 
 app.get("/", async function (req, res) {
-  res.send(`CURRENTLY RUNNING ON ${config.tokens.length} ACCOUNT!`);
+  res.send(`CURRENTLY RUNNING ON ${config.tokens.length} ACCOUNT(S)!`);
 });
 
-app.listen(25669, async () => {
+app.listen(3000, async () => {
   console.log(chalk.bold.bgRed(`SERVER STATUS: ONLINE`));
 });
 
@@ -181,6 +181,21 @@ async function Login(token, Client, guildId) {
 
             setTimeout(async () => {
               isOnBreak = false;
+              now = new Date();
+              if (log)
+                log.send(
+                  new MessageBuilder()
+                    .setTitle("⏯️ ``-`` Resumed")
+                    .setURL("https://github.com/kyan0045/catchtwo")
+                    .setDescription("**Account: **" + client.user.tag)
+                    .setColor("#7ff889")
+                );
+                             console.log(
+            date.format(now, "HH:mm") +
+              `: ` +
+              chalk.red(client.user.username) +
+              `: has resumed `
+          );
             }, sleepTimeInMilliseconds);
 
             const sleepTimeInSeconds = sleepTimeInMilliseconds / 1000;
@@ -191,13 +206,26 @@ async function Login(token, Client, guildId) {
             if (log)
               log.send(
                 new MessageBuilder()
-                  .setTitle("Sleeping")
+                  .setTitle("⏸️ ``-`` Sleeping")
                   .setURL("https://github.com/kyan0045/catchtwo")
                   .setDescription(
-                    `Sleeping for ${roundedSleepTimeInMinutes} minutes.`
+                    "**Account: **" +
+                      client.user.tag +
+                      "\n**Minutes: **" +
+                      roundedSleepTimeInMinutes +
+                      " minutes"
                   )
-                  .setColor("#E74C3C")
+                  .setColor("#EEC60E")
               );
+             now = new Date();
+             console.log(
+            date.format(now, "HH:mm") +
+              `: ` +
+              chalk.red(client.user.username) +
+              `: is sleeping for ` +
+              roundedSleepTimeInMinutes + 
+                 ` minutes`
+          );
           }
         }
       }
@@ -546,7 +574,6 @@ async function Login(token, Client, guildId) {
         fs.appendFile("./data/catches.txt", caught + "\n", (err) => {
           if (err) throw err;
         });
-
       } else if (
         message.content.includes(
           `https://verify.poketwo.net/captcha/${client.user.id}`
@@ -557,7 +584,7 @@ async function Login(token, Client, guildId) {
           date.format(now, "HH:mm") +
             `: ` +
             chalk.red(client.user.username) +
-            `: Encountered a captcha (https://verify.poketwo.net/captcha/${client.user.id})`
+            `: Encountered a captcha ( https://verify.poketwo.net/captcha/${client.user.id} )`
         );
         if (log)
           log.send(
@@ -1763,61 +1790,61 @@ async function Login(token, Client, guildId) {
           try {
             message.react("✅");
 
-        const messages = [
-            "<@716390085896962058>p --n rayquaza --hpiv >23 --atkiv >10 --defiv >23 --spdiv 31",
-            "<@716390085896962058>p --n eternatus --hpiv >25 --defiv >26 --spatkiv >20 --spdefiv >27 --spdiv 31",
-            "<@716390085896962058>p --n groudon --hpiv >23 --atkiv >17 --defiv >17 --spdefiv >26 --spdiv >28",
-            "<@716390085896962058>p --n kyogre --hpiv >25 --defiv >26 --spatkiv >10 --spdiv 31",
-            "<@716390085896962058>p --n dialga --hpiv >15 --spatkiv >19 --spdefiv >18 --spdiv >28",
-            "<@716390085896962058>p --n regigigas --hpiv >10 --atkiv >6 --defiv >6 --spdefiv >11 --spdiv 31",
-            "<@716390085896962058>p --n arceus --hpiv >15 --defiv >16 --spatkiv >20 --spdiv >28",
-            "<@716390085896962058>p --n yveltal --hpiv >23 --atkiv >13 --defiv >9 --spdefiv >17 --spdiv 31",
-            "<@716390085896962058>p --n yveltal --hpiv >23 --atkiv >24 --defiv >23 --spdefiv >17 --spdiv >13",
-            "<@716390085896962058>p --n xerneas --hpiv >15 --atkiv >26 --spdefiv >15 --spdiv >28",
-            "<@716390085896962058>p --n giratina --hpiv >25 --defiv >25 --spdiv >28",
-            "<@716390085896962058>p --n mewtwo --hpiv >24 --defiv >25 --spatkiv >9 --spdiv >28",
-            "<@716390085896962058> p --n Coalossal --n Carkol --n Rolycoly --hp >24 --atk >28 --def >28 --spdef >28 --spatk >28 --spdiv 31",
-            "<@716390085896962058> p --n Slaking --n Vigoroth --n Slakoth --hp >24 --atk >28 --def >28 --spdiv 31",
-            "<@716390085896962058> p --n Metagross --n Metang --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Braviary --n Rufflet --hp >24 --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Primarina --n Brionne --n Popplio --hp >24 --spdef >28 --spdiv 31",
-            "<@716390085896962058> p --n Golisopod --n Wimpod --atk >28 --def >28 --spdiv 31",
-            "<@716390085896962058> p --n Gyarados --n Magikarp --atk >28 --def >28 --spdef >28 --spdiv 31",
-            "<@716390085896962058> p --n Dragonite --n Dragonair --n Dratini --atk >28 --spatk >28 --spdiv 31",
-            "<@716390085896962058> p --n Salamence --n Shelgon --n Bagon --atk >28 --def >28 --spdiv 31",
-            "<@716390085896962058> p --n Skuntank --n Stunky --hp >24 --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Garchomp --n Gabite --hp >24 --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Mega Garchomp --hp >24 --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Delphox --n Braixen --n Fennekin --spatk >28 --spdef >28 --spdiv 31",
-            "<@716390085896962058> p --n Decidueye --n Dartrix --n Rowlet --atk >28 --spatk >28 --spdef >28 --spdiv 31",
-            "<@716390085896962058> p --n Dragapult --n Drakloak --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Gardevoir --n Kirlia --atk >28 --spdef >28 --spdiv 31",
-            "<@716390085896962058> p --n Gengar --n Haunter --atk >28 --spatk >28 --spdiv 31",
-            "<@716390085896962058> p --n Aerodactyl --atk >28 --def >28 --spdiv 31",
-            "<@716390085896962058> p --n Scizor --atk >28 --def >28 --spdef >28",
-            "<@716390085896962058> p --n Blaziken --atk >28 --spdiv 31",
-            "<@716390085896962058> p --n Swampert --atk >28 --def >28 --spdef >28",
-            "<@716390085896962058> p --n Lucario --n Riolu --atk >28 --spatk >28 --spdiv 31",
-            "<@716390085896962058> p --n Gallade --n Kirlia --atk >28 --spdef >28 --spdiv 31",
-            "<@716390085896962058>p --n larvitar --n pupitar --n tyranitar --hpiv >20 --atkiv >25 --defiv >15 --spdefiv >20",
-            "<@716390085896962058>p --n froakie --n frogadier --n greninja --atkiv >14 --spatkiv >23 --spdiv >27",
-            "<@716390085896962058>p --n axew --n fraxure --n haxorus --hpiv >24 --atkiv >23 --spdefiv >24"
-        ];
+            const messages = [
+              "<@716390085896962058>p --n rayquaza --hpiv >23 --atkiv >10 --defiv >23 --spdiv 31",
+              "<@716390085896962058>p --n eternatus --hpiv >25 --defiv >26 --spatkiv >20 --spdefiv >27 --spdiv 31",
+              "<@716390085896962058>p --n groudon --hpiv >23 --atkiv >17 --defiv >17 --spdefiv >26 --spdiv >28",
+              "<@716390085896962058>p --n kyogre --hpiv >25 --defiv >26 --spatkiv >10 --spdiv 31",
+              "<@716390085896962058>p --n dialga --hpiv >15 --spatkiv >19 --spdefiv >18 --spdiv >28",
+              "<@716390085896962058>p --n regigigas --hpiv >10 --atkiv >6 --defiv >6 --spdefiv >11 --spdiv 31",
+              "<@716390085896962058>p --n arceus --hpiv >15 --defiv >16 --spatkiv >20 --spdiv >28",
+              "<@716390085896962058>p --n yveltal --hpiv >23 --atkiv >13 --defiv >9 --spdefiv >17 --spdiv 31",
+              "<@716390085896962058>p --n yveltal --hpiv >23 --atkiv >24 --defiv >23 --spdefiv >17 --spdiv >13",
+              "<@716390085896962058>p --n xerneas --hpiv >15 --atkiv >26 --spdefiv >15 --spdiv >28",
+              "<@716390085896962058>p --n giratina --hpiv >25 --defiv >25 --spdiv >28",
+              "<@716390085896962058>p --n mewtwo --hpiv >24 --defiv >25 --spatkiv >9 --spdiv >28",
+              "<@716390085896962058> p --n Coalossal --n Carkol --n Rolycoly --hp >24 --atk >28 --def >28 --spdef >28 --spatk >28 --spdiv 31",
+              "<@716390085896962058> p --n Slaking --n Vigoroth --n Slakoth --hp >24 --atk >28 --def >28 --spdiv 31",
+              "<@716390085896962058> p --n Metagross --n Metang --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Braviary --n Rufflet --hp >24 --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Primarina --n Brionne --n Popplio --hp >24 --spdef >28 --spdiv 31",
+              "<@716390085896962058> p --n Golisopod --n Wimpod --atk >28 --def >28 --spdiv 31",
+              "<@716390085896962058> p --n Gyarados --n Magikarp --atk >28 --def >28 --spdef >28 --spdiv 31",
+              "<@716390085896962058> p --n Dragonite --n Dragonair --n Dratini --atk >28 --spatk >28 --spdiv 31",
+              "<@716390085896962058> p --n Salamence --n Shelgon --n Bagon --atk >28 --def >28 --spdiv 31",
+              "<@716390085896962058> p --n Skuntank --n Stunky --hp >24 --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Garchomp --n Gabite --hp >24 --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Mega Garchomp --hp >24 --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Delphox --n Braixen --n Fennekin --spatk >28 --spdef >28 --spdiv 31",
+              "<@716390085896962058> p --n Decidueye --n Dartrix --n Rowlet --atk >28 --spatk >28 --spdef >28 --spdiv 31",
+              "<@716390085896962058> p --n Dragapult --n Drakloak --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Gardevoir --n Kirlia --atk >28 --spdef >28 --spdiv 31",
+              "<@716390085896962058> p --n Gengar --n Haunter --atk >28 --spatk >28 --spdiv 31",
+              "<@716390085896962058> p --n Aerodactyl --atk >28 --def >28 --spdiv 31",
+              "<@716390085896962058> p --n Scizor --atk >28 --def >28 --spdef >28",
+              "<@716390085896962058> p --n Blaziken --atk >28 --spdiv 31",
+              "<@716390085896962058> p --n Swampert --atk >28 --def >28 --spdef >28",
+              "<@716390085896962058> p --n Lucario --n Riolu --atk >28 --spatk >28 --spdiv 31",
+              "<@716390085896962058> p --n Gallade --n Kirlia --atk >28 --spdef >28 --spdiv 31",
+              "<@716390085896962058>p --n larvitar --n pupitar --n tyranitar --hpiv >20 --atkiv >25 --defiv >15 --spdefiv >20",
+              "<@716390085896962058>p --n froakie --n frogadier --n greninja --atkiv >14 --spatkiv >23 --spdiv >27",
+              "<@716390085896962058>p --n axew --n fraxure --n haxorus --hpiv >24 --atkiv >23 --spdefiv >24",
+            ];
 
-        let counter = 0;
+            let counter = 0;
 
-        function sendRandomMessage() {
-            if (counter < messages.length) {
+            function sendRandomMessage() {
+              if (counter < messages.length) {
                 const randomDelay = Math.floor(Math.random() * 2000) + 5000;
                 setTimeout(() => {
-                    message.channel.send(messages[counter]);
-                    counter++;
-                    sendRandomMessage();
+                  message.channel.send(messages[counter]);
+                  counter++;
+                  sendRandomMessage();
                 }, randomDelay);
+              }
             }
-        }
 
-          sendRandomMessage();
+            sendRandomMessage();
           } catch (err) {
             console.error(err);
             message.react("❌");
