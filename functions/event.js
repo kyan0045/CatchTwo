1ea3b8539b1a07abab01edf3a68de4374cda57e9
Kyan <84374752+kyan0045@users.noreply.github.com>
2024-08-06 01:13:42 +0200

    v1.4-beta - Add (yet to be tested) ev completer

--- functions/event.js
+++ functions/event.js
@@ -8,9 +8,9 @@ const config = require("../config.json");
 const { sendLog } = require("./logging.js");
 
 async function eventOpen(client, channel_id) {
-    /*const channel = client.channels.cache.get(channel_id);
-    sendLog(client.user.username, `Action sent: opening event boxes`, "debug");
-    channel.send(`<@716390085896962058> event`);*/
+    const channel = client.channels.cache.get(channel_id);
+    sendLog(client.user.username, `Action sent: attempting to play archery.`, "debug");
+    channel.send(`<@716390085896962058> event play archery");
 }
 
 module.exports = { eventOpen };
