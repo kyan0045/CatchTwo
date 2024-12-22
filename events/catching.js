b25e1482b1cdafd2188f8cf8c54f1b0e81634829
Kyan Bosman <84374752+kyan0045@users.noreply.github.com>
2024-12-22 23:11:42 +0100

    v1.4-beta - Add AI handling

--- events/catching.js
+++ events/catching.js
@@ -1,21 +1,65 @@
 // Import necessary modules and functions
-const chalk = require("chalk");
 const { solveHint, getImage, getName } = require("pokehint");
+
 const config = require("../config.js");
+
 const { wait, randomInteger } = require("../utils/utils.js");
 const { ShinyHunter } = require("../classes/clients/shinyHunter.js");
 const {
   sendLog,
   sendCatch,
-  sendWebhook,
-  getMentions,
 } = require("../functions/logging.js");
 const {
   setSpamming,
   getSpamming,
   getWaiting,
-  setWaiting,
 } = require("../utils/states.js");
+const tf = require("@tensorflow/tfjs-node");
+const sharp = require("sharp");
+const data = require("../data/ai.json");
+const axios = require("axios");
+
+let model;
+
+async function predict(url) {
+  if (!model) {
+    model = await tf.loadLayersModel("file://./data/model/model.json");
+  }
+  startTime = new Date().getTime();
+  const imageTensor = await preprocessImage(url);
+  const prediction = model.predict(imageTensor);
+
+  const predictedIndex = prediction.argMax(1).dataSync()[0];
+
+  //console.log(`Predicted class index: ${predictedIndex}`);
+
+  const keys = Object.keys(data); // Get all keys from the data object
+  const name = keys[predictedIndex]; // Get the key name at the specified index
+
+  //console.log(`Predicted class name: ${name}`);
+  sendLog(
+    client.user.username,
+    "AI prediction took " + (new Date().getTime() - startTime) + "ms.",
+    "debug"
+  );
+  return name;
+}
+
+async function preprocessImage(url) {
+  const response = await axios({
+    url,
+    responseType: "arraybuffer",
+  });
+  const imageBuffer = await sharp(Buffer.from(response.data))
+    .resize(64, 64)
+    .toBuffer();
+
+  const imageTensor = tf.divNoNan(tf.node.decodeImage(imageBuffer, 3), 255);
+
+  const expandedTensor = tf.expandDims(imageTensor, 0);
+
+  return expandedTensor;
+}
 
 // Main function to handle message creation events
 module.exports = async (client, guildId, message) => {
@@ -40,11 +84,66 @@ module.exports = async (client, guildId, message) => {
       )
         return;
 
-      // Send a hint message
-      let hintMessages = ["h", "hint"];
-      message.channel.send(
-        "<@716390085896962058> " + hintMessages[Math.round(Math.random())]
-      );
+      if (config.behavior.AI == true) {
+        sendLog(
+          client.user.username,
+          "AI enabled, trying to predict the pokémon.",
+          "debug"
+        );
+        predict(message.embeds[0].image.url)
+          .then(async (result) => {
+            if (
+              config.shinyHunting.HuntPokemons.includes(result.split("\r\n")[0])
+            ) {
+              const shinyHunter = new ShinyHunter(
+                config.shinyHunting.HuntToken
+              );
+              shinyHunter.login();
+              shinyHunter.catch(
+                message.guild.id,
+                message.channel.id,
+                await getName({
+                  name: result.split("\r\n")[0],
+                  inputLanguage: "English",
+                })
+              );
+            } else {
+              pokemonRandom = await getName({
+                name: result.split("\r\n")[0],
+                inputLanguage: "English",
+              });
+              if (!pokemonRandom) {
+                pokemonRandom = result;
+              }
+              checkIfWrong = await message.channel
+                .createMessageCollector({ time: 5000 })
+                .on("collect", async (msg) => {
+                  if (msg.content.includes("That is the wrong pokémon!")) {
+                    checkIfWrong.stop();
+                    setTimeout(async () => {
+                      await msg.channel.send("<@716390085896962058> h ");
+                    }, 500);
+                  }
+                });
+              await message.channel.send(
+                "<@716390085896962058> c " + pokemonRandom
+              );
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            // Send a hint message if the prediction fails
+            let hintMessages = ["h", "hint"];
+            message.channel.send(
+              "<@716390085896962058> " + hintMessages[Math.round(Math.random())]
+            );
+          });
+      } else {
+        let hintMessages = ["h", "hint"];
+        message.channel.send(
+          "<@716390085896962058> " + hintMessages[Math.round(Math.random())]
+        );
+      }
 
       // Handle incense-specific logic
       if (
@@ -143,7 +242,7 @@ module.exports = async (client, guildId, message) => {
         .fetch({ limit: 2, around: message.id })
         .catch(() => null);
       const newMessage = Array.from(messages.values());
-      // [...messages.values()];
+      [...messages.values()];
       if (!newMessage[1]?.content.includes("pick")) return;
       message.clickButton();
       await wait(3000);
@@ -182,120 +281,6 @@ module.exports = async (client, guildId, message) => {
           await getImage(name, shiny)
         );
       }
-    }
-
-    /*if (
-      config.logging.LogCatches &&
-      message.embeds[0]?.footer &&
-      message.embeds[0].footer.text.includes("Displaying")
-    ) {
-      const messages = await message.channel.messages
-        .fetch({ limit: 2, around: message.id })
-        .catch(() => null);
-      const newMessage = Array.from(messages.values());
-      [...messages.values()];
-
-      if (
-        (message.embeds[0].thumbnail.url.includes(client.user.id) ||
-          newMessage[1].author.id == client.user.id) &&
-        newMessage[1].content.includes("i l")
-      ) {
-        const iv = message.embeds[0]?.fields[1].value
-          .split(" ")[28]
-          .substring(
-            0,
-            message.embeds[0]?.fields[1].value.split(" ")[28].length - 1
-          );
-
-        const number = message.embeds[0]?.footer.text
-          .split(" ")[2]
-          .substring(
-            0,
-            message.embeds[0]?.footer.text.split(" ")[2].length - 5
-          );
-
-        if (message.embeds[0].title.includes("✨")) {
-          name = message.embeds[0]?.title.split(" ").slice(3).join(" ");
-          level = message.embeds[0]?.title.split(" ")[2];
-          shiny = true;
-        }
-        name = message.embeds[0]?.title.split(" ").slice(2).join(" ");
-        level = message.embeds[0]?.title.split(" ")[1];
-        shiny = false;
-
-        sendCatch(client.user.username, name, level, iv, number, shiny, message.embeds[0].image.url);
-      }
-    }*/
-  }
-
-  // Handle messages from the bot itself for special cases like chocolate box opening
-  if (message.author.id == "716390085896962058") {
-    
-    // Handle chocolate box opening (past valentines event)
-    /*if (message?.embeds[0]?.fields[0]?.name.includes("Chocolate")) {
-      const messages = await message.channel.messages
-        .fetch({ limit: 2, around: message.id })
-        .catch(() => null);
-      const newMessage = Array.from(messages.values());
-      // [...messages.values()];
-
-      if (newMessage[1].author.id == client.user.id) {
-        let boxAmount = parseInt(message.embeds[0].fields[0].value);
-        openInterval = setInterval(async () => {
-          if (boxAmount > 0) {
-            if (boxAmount >= 15) {
-              message.channel.send(`<@716390085896962058> valentines open 15`);
-              boxAmount -= 15;
-            } else {
-              message.channel.send(
-                `<@716390085896962058> valentines open ${boxAmount}`
-              );
-              boxAmount = 0;
-              clearInterval(openInterval);
-            }
-          }
-        }, 1000);
-      }
-    } else*/ if (
-      message.content.includes(
-        `https://verify.poketwo.net/captcha/${client.user.id}`
-      )
-    ) {
-      // Handle captcha detection and notification
-      if (getWaiting(client.user.username) == true) return;
-      setWaiting(client.user.username, true);
-      sendLog(client.user.username, "Detected captcha.", "captcha");
-      config.ownership.OwnerIDs.forEach((id) => {
-        if (id.length <= 16) return;
-        client.users.fetch(id).then(async (user) => {
-          dmChannel = await client.channels.fetch(user?.dmChannel?.id);
-          if (!dmChannel) {
-            dmChannel = await user.createDM();
-          }
-          lastMessage = await dmChannel.messages.fetch(dmChannel.lastMessageId);
-          if (
-            lastMessage?.content.includes("captcha") &&
-            lastMessage?.author.id == client.user.id &&
-            lastMessage?.createdTimestamp > Date.now() - 86400000
-          ) {
-            return;
-          } else {
-            sendWebhook(await getMentions(), {
-              title: `Captcha Found!`,
-              color: "#FF5600",
-              url: `https://verify.poketwo.net/captcha/${client.user.id}`,
-              footer: {
-                text: "CatchTwo by @kyan0045",
-                icon_url:
-                  "https://cdn.discordapp.com/icons/1133853334944632832/1cb8326e5b0e60e40c8b830803604a6b.png",
-              },
-            });
-            return user.send(
-              `## DETECTED A CAPTCHA\n> I've detected a captcha. The autocatcher has been paused. To continue, please solve the captcha below.\n* https://verify.poketwo.net/captcha/${client.user.id}\n\n### SOLVED?\n> Once solved, run the command \`\`${config.ownership.CommandPrefix}solved\`\` to continue catching.`
-            );
-          }
-        });
-      });
     }
   }
 };
